83fdcee4fd3062f72b855cf7b0485711cc87ea18
sstawiarski <contact@shawnstawiarski.com>
2023-03-09 17:29:48 -0600

    Add unit tests for Alerting and Speech services

---
+++ tests/services/Alerting.spec.ts
@@ -0,0 +1,100 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { AlertingService } from "../../src/services/Alerting";
+import type { IEmailClient } from "../../src/types/services/clients/IEmailClient";
+import type { IPushNotificationClient } from "../../src/types/services/clients/IPushNotificationClient";
+import type { ILogger } from "../../src/types/utils/ILogger";
+import { EmailRequestFactory } from "../testUtils/factories/EmailRequest";
+import { PushNotificationRequestFactory } from "../testUtils/factories/PushNotificationRequest";
+
+describe("AlertingService Test Suite", () => {
+	const loggerInfoFn = vi.fn();
+	const mockLogger = {
+		info: loggerInfoFn
+	} as unknown as ILogger;
+
+	afterEach(() => {
+		loggerInfoFn.mockClear();
+	});
+
+	it("should successfully send email notification", async () => {
+		const mockSendEmailFn = vi.fn().mockResolvedValue(void 0);
+
+		const mockEmailClient = {
+			sendEmail: mockSendEmailFn
+		} as unknown as IEmailClient;
+
+		const alertingService = new AlertingService(mockEmailClient, {} as any, mockLogger);
+		const request = EmailRequestFactory.build();
+
+		await alertingService.sendEmailNotification(request);
+
+		expect(mockSendEmailFn).toHaveBeenCalledTimes(1);
+		expect(loggerInfoFn).toHaveBeenCalledTimes(1);
+		expect(loggerInfoFn).toHaveBeenLastCalledWith("Sending email notification...");
+	});
+
+	it("should throw the original error when send email notification fails", async () => {
+		expect.assertions(4);
+
+		const mockError = new Error("test");
+		const mockSendEmailFn = vi.fn().mockRejectedValue(mockError);
+
+		const mockEmailClient = {
+			sendEmail: mockSendEmailFn
+		} as unknown as IEmailClient;
+
+		const alertingService = new AlertingService(mockEmailClient, {} as any, mockLogger);
+		const request = EmailRequestFactory.build();
+
+		try {
+			await alertingService.sendEmailNotification(request);
+		} catch (error) {
+			expect(error === mockError).toEqual(true);
+		}
+
+		expect(mockSendEmailFn).toHaveBeenCalledTimes(1);
+		expect(loggerInfoFn).toHaveBeenCalledTimes(1);
+		expect(loggerInfoFn).toHaveBeenLastCalledWith("Sending email notification...");
+	});
+
+	it("should successfully send push notification", async () => {
+		const mockSendPushNotificationFn = vi.fn().mockResolvedValue(void 0);
+
+		const mockPushClient = {
+			sendPush: mockSendPushNotificationFn
+		} as unknown as IPushNotificationClient;
+
+		const alertingService = new AlertingService({} as any, mockPushClient, mockLogger);
+		const request = PushNotificationRequestFactory.build();
+
+		await alertingService.sendPushNotification(request);
+
+		expect(mockSendPushNotificationFn).toHaveBeenCalledTimes(1);
+		expect(loggerInfoFn).toHaveBeenCalledTimes(1);
+		expect(loggerInfoFn).toHaveBeenLastCalledWith("Sending push notification...");
+	});
+
+	it("should throw the original error when send email notification fails", async () => {
+		expect.assertions(4);
+
+		const mockError = new Error("test");
+		const mockSendPushFn = vi.fn().mockRejectedValue(mockError);
+
+		const mockPushClient = {
+			sendPush: mockSendPushFn
+		} as unknown as IPushNotificationClient;
+
+		const alertingService = new AlertingService({} as any, mockPushClient, mockLogger);
+		const request = PushNotificationRequestFactory.build();
+
+		try {
+			await alertingService.sendPushNotification(request);
+		} catch (error) {
+			expect(error === mockError).toEqual(true);
+		}
+
+		expect(mockSendPushFn).toHaveBeenCalledTimes(1);
+		expect(loggerInfoFn).toHaveBeenCalledTimes(1);
+		expect(loggerInfoFn).toHaveBeenLastCalledWith("Sending push notification...");
+	});
+});
---
+++ tests/services/Speech.spec.ts
@@ -0,0 +1,46 @@
+import type { SpeechClient } from "@google-cloud/speech";
+import { describe, expect, it, vi } from "vitest";
+import { SpeechService } from "../../src/services/Speech";
+import { TranscriptionResponseFactory } from "../testUtils/factories/TranscriptionResponse";
+
+describe("SpeechService Test Suite", () => {
+	it("should successfully process transcription", async () => {
+		const transcriptionResponse = TranscriptionResponseFactory.build();
+
+		const mockRecognizeFn = vi.fn().mockResolvedValue([transcriptionResponse]);
+		const mockSpeechClient = {
+			recognize: mockRecognizeFn
+		} as unknown as SpeechClient;
+
+		const service = new SpeechService(mockSpeechClient, {} as any);
+
+		const transcription = await service.transcribe("");
+
+		expect(mockRecognizeFn).toHaveBeenCalledTimes(1);
+		expect(transcription).toBeTypeOf("string");
+		expect(transcription.length).not.toBe(0);
+		expect(transcription).toEqual(transcriptionResponse.results!.map((result) => result!.alternatives![0]!.transcript!).join("\n"));
+	});
+
+	it("should throw error when speech client throws error", async () => {
+		expect.assertions(4);
+		const mockLoggerErrorFn = vi.fn();
+
+		const mockRecognizeFn = vi.fn().mockRejectedValue(new Error());
+		const mockSpeechClient = {
+			recognize: mockRecognizeFn
+		} as unknown as SpeechClient;
+
+		const service = new SpeechService(mockSpeechClient, { error: mockLoggerErrorFn } as any);
+
+		try {
+			await service.transcribe("");
+		} catch (error: any) {
+			expect(error).toBeInstanceOf(Error);
+			expect(error.message).toBe("Transcription failed!");
+			expect(mockLoggerErrorFn).toHaveBeenCalledTimes(1);
+		}
+
+		expect(mockRecognizeFn).toHaveBeenCalledTimes(1);
+	});
+});
